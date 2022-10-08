9bc69d466b9d970491d453a6001a56ec6e787de2
JustAStream <keirinvestment@gmail.com>
2022-10-08 18:17:45 +0000

    Release 0.2.0

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,22 @@
-use barter_integration::socket::error::SocketError;
+use crate::model::{order::OrderKind, ClientOrderId};
+use barter_integration::model::Symbol;
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-#[derive(Error, Debug)]
-pub enum ClientError {
-    #[error("Socket error due to: {0}")]
-    SocketError(#[from] SocketError),
+#[derive(Error, PartialEq, Eq, PartialOrd, Debug, Clone, Deserialize, Serialize)]
+pub enum ExecutionError {
+    #[error("failed to build component due to missing attributes: {0}")]
+    BuilderIncomplete(String),
+
+    #[error("SimulatedExchange error: {0}")]
+    Simulated(String),
+
+    #[error("Balance for symbol {0} insufficient to open order")]
+    InsufficientBalance(Symbol),
+
+    #[error("failed to find Order with ClientOrderId: {0}")]
+    OrderNotFound(ClientOrderId),
+
+    #[error("failed to open Order due to unsupported OrderKind: {0}")]
+    UnsupportedOrderKind(OrderKind),
 }
